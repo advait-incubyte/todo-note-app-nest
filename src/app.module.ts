183cca4f72e5f9d5b8124b6b2dac5711fcaf85ad
Advait Maindalkar <advait@incubyte.co>
2025-06-15 18:04:30 +0530

    added notes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, ModuleMetadata } from '@nestjs/common';
+import { NotesModule } from './notes/notes.module';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+const metadata: ModuleMetadata = {
+  imports: [NotesModule],
+}
+@Module(metadata)
 export class AppModule {}
