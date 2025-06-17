2c41a441db98bcb614911b41b8348972817ecf64
Advait Maindalkar <advait@incubyte.co>
2025-06-18 01:17:05 +0530

    feat: injecting drizzlemodule db client into repository

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
 import { NotesRepository } from './notes.repository';
+import { DrizzleModule } from '../drizzle/drizzle.module';
 
 @Module({
+  imports: [DrizzleModule],
   controllers: [NotesController],
   providers: [NotesService, NotesRepository]
 })
--- src/notes/notes.repository.ts
+++ src/notes/notes.repository.ts
@@ -1,11 +1,21 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { CreateNoteDto } from "./dto/create-note.dto";
+import * as schema from "./schema";
+import { DATABASE_CONNECTION } from "../drizzle/constants";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
 
 export const NOTES_REPOSITORY = 'NotesRepository';
 
 @Injectable()
 export class NotesRepository {
+    constructor(@Inject(DATABASE_CONNECTION) private readonly db: NodePgDatabase<typeof schema>) {}
+
     async createNote(createNoteDto: CreateNoteDto) {
-        return Promise.resolve(createNoteDto);
+        const [note] = await this.db
+            .insert(schema.notes)
+            .values(createNoteDto)
+            .returning();
+
+        return note;
     }
 }
