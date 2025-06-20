7b6c54c59edfb0b594e6c6ffcb36aceb73a74123
Advait Maindalkar <advait@incubyte.co>
2025-06-20 06:44:33 +0530

    feat: added cors logic to allow incoming frontend requests

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
 
   await app.listen(process.env.PORT ?? 3000);
 }
